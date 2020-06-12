dd6b38cdca681e5b8088996ea444c2f401f73931
Biloholovskyi <biloholovskyi>
2020-06-12 12:38:43 +0300

    create project

---
+++ app/js/modalForm.js
@@ -0,0 +1,34 @@
+const switchModalType = (e) => {
+  const checkbox = $(e.target);
+  const active = $('.text-active');
+  const disabled = $('.text-disabled');
+
+  checkbox.toggleClass('text-check--tel').toggleClass('text-check--mail');
+  active.toggleClass('text-active').toggleClass('text-disabled');
+  disabled.toggleClass('text-active').toggleClass('text-disabled');
+
+  if(checkbox.hasClass('text-check--tel')) {
+    $('.modal-input[type="text"]').attr('placeholder', 'Укажите номер телефона');
+  } else {
+    $('.modal-input[type="text"]').attr('placeholder', 'Укажите почту');
+  }
+}
+
+const disabledPolitical = () => {
+  $('.political__check').toggleClass('political__check--active');
+}
+
+const closeModalForm = () => {
+  $('.main-modal').fadeOut('slow');
+}
+
+const openModalForm = () => {
+  $('.main-modal').fadeIn('slow').css('display', 'flex');
+}
+
+export {
+  switchModalType,
+  disabledPolitical,
+  closeModalForm,
+  openModalForm
+}
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const webpack = require('webpack-stream');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const pug = require('gulp-pug');
+
+const webConfig = {
+  mode: 'production',
+  output: {
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+};
+
+gulp.task('pug', function () {
+  return gulp.src('app/pug/pages/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('app/build'));
+})
+
+gulp.task('default', function() {
+  browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
+    gulp.watch("app/pug/**/*.pug", function () {
+        return gulp.src('app/pug/pages/*.pug')
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(gulp.dest('app/'))
+            .pipe(browserSync.reload({stream: true}));
+    });
+    gulp.watch("app/sass/**/*.scss", function () {
+        return gulp.src("app/sass/**/*.scss")
+          .pipe(sass())
+          .pipe(autoprefixer({
+            overrideBrowserslist: ['> 0.1%'],
+            cascade: false
+          }))
+          .pipe(cleanCSS({
+            level: 2
+          }))
+          .pipe(gulp.dest("app/css"))
+          .pipe(browserSync.stream());
+    });
+    gulp.watch("app/js/**/*.js", function () {
+      return gulp.src("app/js/index.js")
+        .pipe(webpack(webConfig))
+        .pipe(gulp.dest("app/buildjs"))
+        .pipe(browserSync.stream());
+    });
+});
